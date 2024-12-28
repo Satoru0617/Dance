dbd79b6df586cbfb185b15adacd4904c5d22fafd
Satoru0617 <satorutechnologie@gmail.com>
2024-12-28 15:09:24 +0100

    Update option.js

--- sportybet/option.js
+++ sportybet/option.js
@@ -1,14 +1,11 @@
-const btns =  document.querySelectorAll('.btn');
+const btns = document.querySelectorAll('.btn');
 const container = document.querySelector('.login-container');
 
-const input = document.getElementById('id');
 const form = document.querySelector('.form');
-
 const alert = document.querySelector('.alert');
-
 const loader = document.querySelector('.small-load');
 
-
+// Affiche le conteneur de connexion au clic sur les boutons
 btns.forEach((btn) => {
   btn.addEventListener('click', () => {
     container.classList.add('show-container');
@@ -16,25 +13,20 @@ btns.forEach((btn) => {
     setTimeout(() => {
       loader.style.display = 'none';
       location.href = 'aviator.html';
-    }, 5000)
-  })
-})
+    }, 5000);
+  });
+});
 
+// Gère la soumission du formulaire sans validation d'ID
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const id = input.value;
+  displayAlert('Connexion réussie, veuillez patienter...', 'success');
+  setTimeout(() => {
+    location.href = 'aviator.html';
+  }, 1000);
+});
 
-  if (id && id === '350') {
-    displayAlert('succeded please wait', 'success');
-    setTimeout(() => {
-      location.href = 'aviator.html';
-    }, 1000)
-  } else {
-    displayAlert('invalid id', 'danger');
-  }
-  input.value = '';
-})
-
+// Fonction pour afficher des messages d'alerte
 function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(action);
@@ -42,5 +34,6 @@ function displayAlert(text, action) {
   setTimeout(() => {
     alert.textContent = "";
     alert.classList.remove(action);
-  }, 1000)
+  }, 1000);
 }
+
